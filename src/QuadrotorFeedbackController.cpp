583887181bdd346b1ea84370d29455c8aefaa5b2
wyks <847502343@qq.com>
2023-09-08 17:30:49 +0800

    save may success

--- src/QuadrotorFeedbackController.cpp
+++ src/QuadrotorFeedbackController.cpp
@@ -17,19 +17,12 @@ QuadrotorFeedbackController::QuadrotorFeedbackController(geometry_msgs::PoseStam
   eval_ptr_ = 0;
 
   // hover PID params init
-  kp_hover_x_ = 1.5;
-  kp_hover_y_ = 1.5;
-<<<<<<< HEAD
-  kp_hover_z_ = 0.9;
+  kp_hover_x_ = 1.4;
+  kp_hover_y_ = 1.4;
+  kp_hover_z_ = 1.05;
   kp_hover_vx_ = 0.22;
   kp_hover_vy_ = -0.22;
-  kp_hover_vz_ = 0.052;
-=======
-  kp_hover_z_ = 1.0;
-  kp_hover_vx_ = 0.22;
-  kp_hover_vy_ = -0.22;
-  kp_hover_vz_ = 0.06;
->>>>>>> save in 9.8
+  kp_hover_vz_ = 0.07;
 
   // ki_hover_x_ = 0;
   // ki_hover_y_ = 0;
@@ -37,21 +30,12 @@ QuadrotorFeedbackController::QuadrotorFeedbackController(geometry_msgs::PoseStam
   // ki_hover_vx_ = 0;
   // ki_hover_vy_ = 0;
   // ki_hover_vz_ = 0;
-<<<<<<< HEAD
-  ki_hover_x_ = 0.11;
-  ki_hover_y_ = 0;
-  ki_hover_z_ = 0.01;
-  ki_hover_vx_ = 0.11;
-  ki_hover_vy_ = -0.1;
-  ki_hover_vz_ = 0.03;
-=======
   ki_hover_x_ = 0.05;
   ki_hover_y_ = 0.05;
-  ki_hover_z_ = 0.04;
+  ki_hover_z_ = 0.03;
   ki_hover_vx_ = 0.1;
   ki_hover_vy_ = -0.1;
   ki_hover_vz_ = 0.1;
->>>>>>> save in 9.8
 
   kd_hover_x_ = 0;
   kd_hover_y_ = 0;
@@ -64,274 +48,6 @@ QuadrotorFeedbackController::QuadrotorFeedbackController(geometry_msgs::PoseStam
 QuadrotorFeedbackController::~QuadrotorFeedbackController(){
 }
 
-<<<<<<< HEAD
-void QuadrotorFeedbackController::positionControlFeedback(){
-  Eigen::Vector3d position_cmd_(position_setpoint_.pose.position.x, position_setpoint_.pose.position.y, position_setpoint_.pose.position.z);
-  Eigen::Vector3d position_error_ = position_cmd_ - current_position_;
-  
-  double psi_ = current_attitude_[2];
-  Eigen::Matrix3d R_E_B_;
-  R_E_B_ << cos(psi_),sin(psi_),ZERO,
-          -sin(psi_),cos(psi_),ZERO,
-          ZERO,ZERO,ONE;
-  position_error_ = R_E_B_ * position_error_;
-  
-
-  // if(current_state_== OFFBOARD)
-    
-  // else
-    // position_error_sum_ = Eigen::Vector3d(0,0,0);
-  // std::cout<<"!!!!!!!!!!"<<position_error_[2]<<" "<<position_error_sum_[2]<<std::endl;
-  double ki_hover_x_use;
-  double ki_hover_y_use;
-  double ki_hover_z_use;
-   //if((ros::Time::now() - data_ptr->begin_time).toSec() > 3.0f){
-     position_error_sum_ += position_error_ / LOOP_FREQUENCY;
-      ki_hover_x_use = ki_hover_x_;
-      ki_hover_y_use = ki_hover_y_;
-     ki_hover_z_use = ki_hover_z_;
-  //}
-   //else{
-   // ki_hover_x_use = 0;
-    // ki_hover_y_use = 0;
-    // ki_hover_z_use = 0;
-    // std::cout<<"Second : "<<(ros::Time::now() - data_ptr->begin_time).toSec()<<std::endl;
-  // }
-  
-  double position_i_x_ = ki_hover_x_use * position_error_sum_[0]; 
-  if(position_i_x_ > 5)
-    position_i_x_ = 5;
-  if(position_i_x_ < -5)
-    position_i_x_ = -5;
-
-  double position_i_y_ = ki_hover_y_use * position_error_sum_[1]; 
-  if(position_i_y_ > 5)
-    position_i_y_ = 5;
-  if(position_i_y_ < -5)
-    position_i_y_ = -5;
-
-  double position_i_z_ = ki_hover_z_use * position_error_sum_[2]; 
-  if(position_i_z_ > 10)
-    position_i_z_ = 10;
-  if(position_i_z_ < -10)
-    position_i_z_ = -10;
-
-  position_int.vector.x = position_i_x_;
-  position_int.vector.y = position_i_y_;
-  position_int.vector.z = position_i_z_;
- /*ROS_INFO("the position_i_x_is %f",position_i_x_ );
-  ROS_INFO("the position_i_y_is %f",position_i_y_ );
-  ROS_INFO("the position_i_z_is %f",position_i_z_ );*/
-  double position_d_x_=kd_hover_x_*(-position_error_[0] + position_error_before_[0])*LOOP_FREQUENCY;
-  if(position_d_x_ > 5)
-    position_d_x_ = 5;
-  if(position_d_x_ < -5)
-    position_d_x_ = -5;
-
-  double position_d_y_=kd_hover_y_*(-position_error_[1] + position_error_before_[1])*LOOP_FREQUENCY;
-  if(position_d_y_ > 5)
-    position_d_y_ = 5;
-  if(position_d_x_ < -5)
-    position_d_x_ = -5;
-
-  double position_d_z_=kd_hover_z_*(-position_error_[2] + position_error_before_[2])*LOOP_FREQUENCY;
-  if(position_d_y_ > 10)
-    position_d_y_ = 10;
-  if(position_d_x_ < -10)
-    position_d_x_ = -10;
-
-  position_error_before_=position_error_;
-  
-
-  //velocity_setpoint_.twist.linear.x = kp_hover_x_ * position_error_[0]+ position_i_x_;
-  //velocity_setpoint_.twist.linear.y = kp_hover_y_ * position_error_[1]+ position_i_y_;
-  velocity_setpoint_.twist.linear.x = kp_hover_x_ * position_error_[0]+ position_i_x_+position_d_x_;
-  velocity_setpoint_.twist.linear.y = kp_hover_y_ * position_error_[1]+ position_i_y_+position_d_y_;
-  velocity_setpoint_.twist.linear.z = kp_hover_z_ * position_error_[2] + position_i_z_+position_d_z_;
-  // velocity_setpoint_.twist.linear.x = 1;
-  // velocity_setpoint_.twist.linear.y = 0.5;
-  // velocity_setpoint_.twist.linear.z = 0.2;
-
-  position_setpoint_.header.stamp = ros::Time::now();
-  position_setpoint_.header.frame_id = "odom";
-  velocity_setpoint_.header.stamp = ros::Time::now();
-  velocity_setpoint_.header.frame_id = "odom";
-
-  data_ptr->pub_setpoint_position_ = position_setpoint_;
-  data_ptr->pub_setpoint_velocity_ = velocity_setpoint_;
-  data_ptr->pub_position_int=position_int;
-
-  // std::cout << data_ptr->pub_setpoint_position_ << std::endl;
-  
-}
-
-void QuadrotorFeedbackController::velocityControlFeedback(){
-  Eigen::Vector3d velocity_cmd_(velocity_setpoint_.twist.linear.x, velocity_setpoint_.twist.linear.y, velocity_setpoint_.twist.linear.z);
-  // std::cout<<"!!!"<<velocity_cmd_<<std::endl;
-  Eigen::Vector3d velocity_error_  = velocity_cmd_ - current_velocity_;
-
-  double psi_ = current_attitude_[2];
-  // double psi_ =  0.0;
-  /*Eigen::Matrix3d R_E_B_;
-  R_E_B_ << cos(psi_),sin(psi_),ZERO,
-          -sin(psi_),cos(psi_),ZERO,
-          ZERO,ZERO,ONE;
-  velocity_error_ = R_E_B_ * velocity_error_;*/
-  
-  // if(current_state_ == OFFBOARD)
-    
-  // else
-    // velocity_error_sum_ = Eigen::Vector3d(0,0,0); 
-
-  double ki_hover_vz_use;
-  double ki_hover_vy_use;
-  double ki_hover_vx_use;
-  // if((ros::Time::now() - data_ptr->begin_time).toSec() > 5.0f){
-    velocity_error_sum_ += velocity_error_ / LOOP_FREQUENCY;
-      ki_hover_vz_use = ki_hover_vz_;
-      ki_hover_vy_use = ki_hover_vy_;
-      ki_hover_vx_use = ki_hover_vx_;
-  // }
-  // else{
-  //     ki_hover_vz_use = 0;
-  //     ki_hover_vy_use = 0;
-  //     ki_hover_vx_use = 0;
-  // }
-  double velocity_i_x_ = ki_hover_vx_use * velocity_error_sum_[0]; 
-  if(velocity_i_x_ > 0.8)
-    velocity_i_x_ = 0.8;
-  if(velocity_i_x_ < -0.8)
-    velocity_i_x_ = -0.8; 
-
-  double velocity_i_y_ = ki_hover_vy_use * velocity_error_sum_[1]; 
-  if(velocity_i_y_ > 0.8)
-    velocity_i_y_ = 0.8;
-  if(velocity_i_y_ < -0.8)
-    velocity_i_y_ = -0.8; 
-
-  double velocity_i_z_ = ki_hover_vz_use * velocity_error_sum_[2]; 
-  if(velocity_i_z_ > 1)
-    velocity_i_z_ = 1;
-  if(velocity_i_z_ < -1)
-    velocity_i_z_ = -1;
-
-  double velocity_d_x_ = kd_hover_vx_* (-velocity_error_[0] + velocity_error_before_[0])*LOOP_FREQUENCY; 
-  if(velocity_d_x_ > 0.8)
-    velocity_d_x_ = 0.8;
-  if(velocity_d_x_ < -0.8)
-    velocity_d_x_ = -0.8; 
-
-  double velocity_d_y_ = kd_hover_vy_ *  (-velocity_error_[1] + velocity_error_before_[1])*LOOP_FREQUENCY; 
-  if(velocity_d_y_ > 0.8)
-    velocity_d_y_ = 0.8;
-  if(velocity_d_y_ < -0.8)
-    velocity_d_y_ = -0.8; 
-
-  double velocity_d_z_ = kd_hover_vz_ * (-velocity_error_[2] + velocity_error_before_[2])*LOOP_FREQUENCY; 
-  if(velocity_d_z_ > 1)
-    velocity_d_z_ = 1;
-  if(velocity_d_z_ < -1)
-    velocity_d_z_ = -1;
-
-  velocity_int.vector.x = velocity_i_x_;
-  velocity_int.vector.y = velocity_i_y_;
-  velocity_int.vector.z = velocity_i_z_;
-
-  data_ptr->pub_velocity_int=velocity_int;
-
-velocity_error_before_ = velocity_error_;
-
-  double thrust_cmd_ = 0.380+ kp_hover_vz_ * velocity_error_[2] + velocity_i_z_+velocity_d_z_;
-  if(thrust_cmd_ >= 0.65)
-    thrust_cmd_ = 0.65;
-  if(thrust_cmd_ <= 0.01)
-    thrust_cmd_ = 0;
-  // printf("thrust_cmd: %lf \n", thrust_cmd_);
-  if(current_position_(2)<0)
-  {
-      ROS_INFO("the t265 is wrong");
-     // printf("z is%f", cur_position_(2));
-      thrust_cmd_ = 0.380;
-
-  }
-  double theta_cmd_ = kp_hover_vx_ * velocity_error_[0]+  velocity_i_x_+ velocity_d_x_;
-  
-  // double theta_cmd_ =0 / 180 *PI;
-  double phi_cmd_ = kp_hover_vy_ * velocity_error_[1] +  velocity_i_y_+ velocity_d_y_;
-  // double phi_cmd_ = 0 / 180 *PI;
-  double psi_cmd_ = 0.0f;
-  if(theta_cmd_ > 0.5)
-    theta_cmd_ = 0.5;
-  if(theta_cmd_ < -0.5)
-    theta_cmd_ = -0.5;
-  if(phi_cmd_ > 0.5)
-    phi_cmd_ = 0.5;
-  if(phi_cmd_ < -0.5)
-    phi_cmd_ = -0.5;
-
-  data_ptr->thrust_eval[eval_ptr_] = thrust_cmd_;
-  if(eval_ptr_ == 4)
-    eval_ptr_ = 0;
-  else
-    eval_ptr_ += 1;
-
-  double yaw_diff = psi_cmd_ - psi_;
-
-   if(yaw_diff > 3)
-    yaw_diff = psi_cmd_ - psi_ - 6.2832;
-  
-  if(yaw_diff < -3)
-    yaw_diff = 6.2832 + psi_cmd_ - psi_;
-  
-  double yaw_rate_cmd = 6.0f * (yaw_diff);
-  //tf::Quaternion oq_;
-  //oq_.setRPY(phi_cmd_, theta_cmd_, psi_cmd_ - psi_);
-  // thrust_attitude_cmd_.orientation.w = oq_.w();
-  // thrust_attitude_cmd_.orientation.x = oq_.x();
-  // thrust_attitude_cmd_.orientation.y = oq_.y();
-  // thrust_attitude_cmd_.orientation.z = oq_.z();
-  thrust_attitude_cmd_.thrust = thrust_cmd_;
-  // thrust_attitude_cmd.body_rate = Vector3d(0,0,0);
-  thrust_attitude_cmd_.type_mask = 7;
-  // dirty fix, sent attitude command during rate channel to aviod betaflight yaw rate control bug.
-  thrust_attitude_cmd_.body_rate.x = phi_cmd_;
-  thrust_attitude_cmd_.body_rate.y = theta_cmd_;
-  thrust_attitude_cmd_.body_rate.z = yaw_rate_cmd;
-  // thrust_attitude_cmd_.body_rate.x = 10;
-  // thrust_attitude_cmd_.body_rate.y = 20;
-  // thrust_attitude_cmd_.body_rate.z = 30;
-  // thrust_attitude_cmd_.thrust = thrust_cmd_;
-  // thrust_attitude_cmd_.body_rate.x = 0;
-  // thrust_attitude_cmd_.body_rate.y = 0.3 * sin((ros::Time::now() - data_ptr->begin_time).toSec() * 3.14 * 2);
-  // thrust_attitude_cmd_.body_rate.z = 0;
-  // thrust_attitude_cmd_.type_mask = 128;
-
-  
-  data_ptr->thrust_attitude_cmd_ = thrust_attitude_cmd_;
-
-  data_ptr->pub_euler_attitude_.vector.x = current_attitude_[0]*180/PI;
-  data_ptr->pub_euler_attitude_.vector.y = current_attitude_[1]*180/PI;
-  data_ptr->pub_euler_attitude_.vector.z = current_attitude_[2]*180/PI;
-  data_ptr->pub_euler_attitude_.header.stamp = ros::Time::now();
-
-  data_ptr->pub_setpoint_attitude_.vector.x = phi_cmd_*180/PI;
-  data_ptr->pub_setpoint_attitude_.vector.y = theta_cmd_*180/PI;
-  data_ptr->pub_setpoint_attitude_.vector.z = psi_cmd_*180/PI;
-  data_ptr->pub_setpoint_attitude_.header.stamp = ros::Time::now();
-
-  data_ptr->pub_new_velocity.header.stamp = ros::Time::now();
-  data_ptr->pub_new_velocity.vector.x = current_velocity_[0];
-  data_ptr->pub_new_velocity.vector.y = current_velocity_[1];
-  data_ptr->pub_new_velocity.vector.z = current_velocity_[2];
-
-
-  // std::cout << "phi_cmd: " << phi_cmd_*180/PI << std::endl; 
-  // std::cout << "theta_cmd: " << theta_cmd_*180/PI << std::endl; 
-  // std::cout << "psi_cmd: " << psi_cmd_*180/PI << std::endl;
-}
-
-=======
->>>>>>> save in 9.8
 void QuadrotorFeedbackController::positionPlanningFeedback(geometry_msgs::PoseStamped position_setpoint){
   position_setpoint_ = position_setpoint;
   Eigen::Vector3d position_cmd_(position_setpoint_.pose.position.x, position_setpoint_.pose.position.y, position_setpoint_.pose.position.z);
@@ -383,13 +99,10 @@ void QuadrotorFeedbackController::positionPlanningFeedback(geometry_msgs::PoseSt
   if(position_i_z_ < -10)
     position_i_z_ = -10;
 
-<<<<<<< HEAD
-=======
   position_int.vector.x = position_i_x_;
   position_int.vector.y = position_i_y_;
   position_int.vector.z = position_i_z_;
 
->>>>>>> save in 9.8
 double position_d_x_=kd_hover_x_*(-position_error_[0] + position_error_before_[0])*LOOP_FREQUENCY;
   if(position_d_x_ > 5)
     position_d_x_ = 5;
@@ -447,7 +160,7 @@ void QuadrotorFeedbackController::velocityPlanningFeedback(double psi_cmd){
     
   // else
     // velocity_error_sum_ = Eigen::Vector3d(0,0,0); 
-  velocity_error_[2] = velocity_error_[2] -0.15;
+  velocity_error_[2] = velocity_error_[2] -0.05;
   double ki_hover_vz_use;
   double ki_hover_vy_use;
   double ki_hover_vx_use;
@@ -480,14 +193,11 @@ void QuadrotorFeedbackController::velocityPlanningFeedback(double psi_cmd){
   if(velocity_i_z_ < -1)
     velocity_i_z_ = -1;
 
-<<<<<<< HEAD
-=======
   velocity_int.vector.x = velocity_i_x_;
   velocity_int.vector.y = velocity_i_y_;
   velocity_int.vector.z = velocity_i_z_;
 
 
->>>>>>> save in 9.8
     double velocity_d_x_ = kd_hover_vx_* (-velocity_error_[0] + velocity_error_before_[0])*LOOP_FREQUENCY; 
   if(velocity_d_x_ > 0.8)
     velocity_d_x_ = 0.8;
